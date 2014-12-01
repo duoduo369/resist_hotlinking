df9bf207eec15885f8e712c85e7a73a2c0e34d6b
duoduo369 <duoduo3369@gmail.com>
2014-12-01 15:37:06 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from os.path import join, dirname
 from setuptools import setup
 
-PACKAGE_NAME = ''
-PACKAGE_PATH = ''
+PACKAGE_NAME = 'resist_hotlinking'
+PACKAGE_PATH = 'resist_hotlinking'
 
 version = __import__(PACKAGE_PATH).__version__
 
-SHORT_DESCRIPTION = ''''''
+SHORT_DESCRIPTION = '''python curl反防盗链抓取连接'''
 
-LONG_DESCRIPTION = ''''''
+LONG_DESCRIPTION = '''python curl反防盗链抓取连接'''
 
 def long_description():
     """Return long description from README.md if it's present
@@ -27,12 +27,12 @@ setup(name=PACKAGE_NAME,
       author_email='duoduo3369@gmail.com',
       description= SHORT_DESCRIPTION,
       license='MIT',
-      keywords='',
-      url='',
-      download_url='',
-      packages=[''],
+      keywords='反防盗链',
+      url='https://github.com/duoduo369/resist_hotlinking',
+      download_url='https://github.com/duoduo369/resist_hotlinking/archive/v0.1.0.tar.gz',
+      packages=['resist_hotlinking'],
       long_description=long_description(),
-      install_requires=[],
+      install_requires=['pycurl'],
       classifiers=[
                    'Development Status :: 4 - Beta',
                    'Topic :: Internet',
